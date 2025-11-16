5acf48d0a7dc7af1d6babf71eec36a884977317c
Ducanh <ducanhtu0@gmail.com>
2025-11-16 18:35:48 +0700

    Update config and build to /docs folder

--- vite.config.ts
+++ vite.config.ts
@@ -3,21 +3,32 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
-    return {
-      server: {
-        port: 3000,
-        host: '0.0.0.0',
-      },
-      plugins: [react()],
-      define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-      },
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
+  const env = loadEnv(mode, '.', '');
+  return {
+    
+    // === CẤU HÌNH THÊM CHO GITHUB PAGES ===
+    // 1. Đặt đường dẫn cơ sở theo tên repository
+    base: '/techcom/',
+    
+    // 2. Đặt tên thư mục build là 'docs'
+    build: {
+      outDir: 'docs'
+    },
+    // =====================================
+
+    server: {
+      port: 3000,
+      host: '0.0.0.0',
+    },
+    plugins: [react()],
+    define: {
+      'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
+      'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
+    },
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, '.'),
       }
-    };
+    }
+  };
 });
