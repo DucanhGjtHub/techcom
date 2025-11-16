dacd867bd2b84f8b03b2d13c548b7ff9f8dddeec
Ducanh <ducanhtu0@gmail.com>
2025-11-16 19:28:38 +0700

    Install terser and fix legacy build

--- vite.config.ts
+++ vite.config.ts
@@ -2,25 +2,34 @@ import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// 1. THÊM DÒNG NÀY ĐỂ IMPORT PLUGIN
+import legacy from '@vitejs/plugin-legacy';
+
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
   return {
     
-    // === CẤU HÌNH THÊM CHO GITHUB PAGES ===
-    // 1. Đặt đường dẫn cơ sở theo tên repository
+    // === CẤU HÌNH GITHUB PAGES ===
     base: '/techcom/',
-    
-    // 2. Đặt tên thư mục build là 'docs'
     build: {
       outDir: 'docs'
     },
-    // =====================================
+    // =============================
 
     server: {
       port: 3000,
       host: '0.0.0.0',
     },
-    plugins: [react()],
+    
+    plugins: [
+      react(),
+      
+      // 2. THÊM PLUGIN VÀO ĐÂY ĐỂ HỖ TRỢ TRÌNH DUYỆT CŨ/DI ĐỘNG
+      legacy({
+        targets: ['defaults', 'not IE 11']
+      })
+    ],
+
     define: {
       'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
